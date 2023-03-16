0e37ba640c6220f1aad5bb616e5ec94c177d8d08
Ahmed <ahmed.tarek540540@gmail.com>
2023-03-16 11:36:50 -0700

    all

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - write a line with printf
+ * Return: 0 if (sucess)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}

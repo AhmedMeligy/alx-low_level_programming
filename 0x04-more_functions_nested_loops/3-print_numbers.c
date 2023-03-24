f377c31ac82c6275cf2f672d159a1ef498267c1e
Ahmed <ahmed.tarek540540@gmail.com>
2023-03-24 16:33:47 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print form 0-9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c < 10 ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
